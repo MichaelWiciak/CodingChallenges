3b3a58f80c93f6b78c0b2b2ff05b0574924b3116
Michael Wiciak <86964604+MichaelWiciak@users.noreply.github.com>
2024-08-15 14:56:08 +0100

    Refactor

---
+++ CodingChallenges/CodeWars/LastDigitOfLargeNumber/lastDigit.py
@@ -0,0 +1,5 @@
+def last_digit(lst):
+    # lst is [x1, x2, x3, ..., xn]
+
+    # get the last 2 digits from lst
+    
--- CodingChallenges/CodeWars/MusicEncoder/compress.py
+++ CodingChallenges/CodeWars/MusicEncoder/compress.py
@@ -1,5 +1,6 @@
 def compress(raw):
 
+    # If the input is empty, return an empty string
     if not raw:
         return ""
 
@@ -9,26 +10,32 @@ def compress(raw):
 
     # Iterate through the raw list
     while i < n:
-        # Identical numbers
+        # case 1: identical numbers
         j = i
         # Find the end of the identical numbers
         while j < n and raw[j] == raw[i]:
             j += 1
+        # If there are more than 2 identical numbers
         if j - i > 1:
             compressed.append(f"{raw[i]}*{j - i}")
+            # Move the pointer to the next number (i=j because j is the number that doesn't match)
             i = j
             continue
 
-        # Consecutive numbers (ascending or descending)
+        # case 2: consecutive numbers
         j = i
+        # Find the end of the consecutive numbers
         while j + 1 < n and raw[j + 1] - raw[j] == raw[i + 1] - raw[i]:
             j += 1
+        # If there are more than 2 consecutive numbers
         if j - i >= 2:
             interval = raw[i + 1] - raw[i]
+            # If the interval is 1 or -1, don't include the interval
             if interval == 1 or interval == -1:
                 compressed.append(f"{raw[i]}-{raw[j]}")
             else:
                 compressed.append(f"{raw[i]}-{raw[j]}/{abs(interval)}")
+            # Move the pointer to the next number (i=j+1 because j is the last number in the sequence)
             i = j + 1
             continue
 
@@ -45,6 +52,7 @@ def test():
         ("3 consecutive numbers, ascending", [1, 3, 4, 5, 7], "1,3-5,7"),
         ("3 consecutive numbers, descending", [1, 5, 4, 3, 7], "1,5-3,7"),
         ("3 numbers with same interval, descending", [1, 10, 8, 6, 7], "1,10-6/2,7"),
+        ("x", [1, 4, 7, 10, 13, 15, 16, 17, 18, 19, 20, 25], "1-13/3,15-20,25"),
     )
 
     for name, raw, expected in TESTS:
